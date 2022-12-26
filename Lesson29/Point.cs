b75e9887e7ee47b2b6b97e4e6f6468235e5b471e
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2022-12-26 23:29:27 +0200

    Добавьте файлы проекта.

---
+++ Lesson29/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson29
+{
+     class Point
+    {
+        public int X { get; set; }
+
+        public Point Y { get; set; }
+
+        public Point Clone()
+        {
+            return MemberwiseClone() as Point;
+        }
+       public override bool Equals(object obj)
+        {
+            if(obj is Point point)
+            {
+                return point.X == X;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public override int GetHashCode()
+        {
+            return X;
+        }
+        public override string ToString()
+        {
+            return X.ToString();
+        }
+
+        public new  Type GetType()
+        {
+            return typeof(UInt16); 
+        }
+
+    }
+}
---
+++ Lesson29/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Lesson29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = new object();
+            int i = 5;
+            int j = 5;
+            Console.WriteLine(i.Equals(j));
+
+            object oi = (object)i;
+            object oj = (object)j;
+            Console.WriteLine(oi.Equals(oj));
+            var p1 = new Point() { X = 5 };
+            var p2 = new Point() { X = 5 };
+            Console.WriteLine(p1.Equals(p2));
+            Console.WriteLine(i.GetHashCode());
+            Console.WriteLine(j.GetHashCode());
+            Console.WriteLine(new MyClass().GetHashCode());
+            Console.WriteLine(p1.GetHashCode());
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p1.GetType());
+            var pp = new Point() { X = 7, Y = new Point()};
+            var pp2 = pp;
+            pp2.X = 77;
+            pp2.Y = new Point() { X = 99};
+            Console.WriteLine(pp);
+            var pp3 = pp.Clone();
+            pp3.X = 88;
+            pp3.Y.X = 222;
+            Console.WriteLine(pp3);
+            Console.WriteLine(pp);
+            Console.WriteLine(pp.Y);
+             Console.ReadLine();
+        }
+    }
+}
